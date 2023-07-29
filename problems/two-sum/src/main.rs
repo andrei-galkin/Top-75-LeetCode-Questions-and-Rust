f3cdac8ab48bb82e68a9d33193dfac7d6eabd359
andrei-galkin <andron.galkin@gmail.com>
2023-07-29 00:25:01 -0400

    Solution for two-sum

--- problems/two-sum/src/main.rs
+++ problems/two-sum/src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 fn main() {
-    println!("Expected: [0, 1] Output: {:?}", two_sum(vec![2, 7, 11, 15], 9));
-    println!("Expected: [1, 2] Output: {:?}", two_sum(vec![3,2,4], 6));
-    println!("Expected: [0, 1] Output: {:?}", two_sum(vec![3,3], 6));
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![3,2,4], 6), vec![1, 2]);
+    assert_eq!(two_sum(vec![3,3], 6), vec![0, 1]);
 }
 
 fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
